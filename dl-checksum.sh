7322cb8b0f6c13f6bf42952675644c2282833b53
Andrew Rothstein <andrew.rothstein@gmail.com>
2018-03-25 16:08:13 -0400

    up to v0.10.0

---
+++ dl-checksum.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+VER=v0.10.0
+DIR=~/Downloads
+MIRROR=https://github.com/coreos/flannel/releases/download
+for PLATFORM in linux-amd64 linux-arm linux-arm64 linux-ppc64le windows-amd64
+do
+    DIST=flannel-$VER-$PLATFORM.tar.gz
+    wget -O $DIR/$DIST $MIRROR/$VER/$DIST
+done
+sha256sum $DIR/flannel-$VER-*.tar.gz
+#shasum -a 256 $DIR/flannel-*-$VER
